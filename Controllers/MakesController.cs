5234afe75be1527cc3d65418b91f1077f9b58103
Omar <omar.nabolsi@gmail.com>
2018-03-03 09:39:32 -0800

    Building an API

---
+++ Controllers/MakesController.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using vega.Data;
+using vega.Models;
+
+namespace vega.Controllers
+{
+    public class MakesController : Controller
+    {
+        private readonly VegaDbContext context;
+        public MakesController(VegaDbContext context)
+        {
+            this.context = context;
+
+        }
+        
+        [HttpGet("/api/makes")]
+        public async Task<IEnumerable<Make>> GetMakes()
+        {
+            return await context.Makes.Include(m => m.Models).ToListAsync();
+        }
+
+    }
+}
