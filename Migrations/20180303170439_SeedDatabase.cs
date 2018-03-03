cb93e4dd66354e280e07571b7b76f136921cdfe9
Omar <omar.nabolsi@gmail.com>
2018-03-03 09:16:38 -0800

    Seeding the Database with data

---
+++ Migrations/20180303170439_SeedDatabase.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace vega.Migrations
+{
+    public partial class SeedDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('Make1')");
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('Make2')");
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('Make3')");
+            
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make1-ModelA', (SELECT TOP(1) ID FROM Makes WHERE Name='Make1'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make1-ModelB', (SELECT TOP(1) ID FROM Makes WHERE Name='Make1'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make1-ModelC', (SELECT TOP(1) ID FROM Makes WHERE Name='Make1'))");
+            
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make2-ModelA', (SELECT TOP(1) ID FROM Makes WHERE Name='Make2'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make2-ModelB', (SELECT TOP(1) ID FROM Makes WHERE Name='Make2'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make2-ModelC', (SELECT TOP(1) ID FROM Makes WHERE Name='Make2'))");
+            
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make3-ModelA', (SELECT TOP(1) ID FROM Makes WHERE Name='Make3'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make3-ModelB', (SELECT TOP(1) ID FROM Makes WHERE Name='Make3'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make3-ModelC', (SELECT TOP(1) ID FROM Makes WHERE Name='Make3'))");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Makes");
+        }
+    }
+}
