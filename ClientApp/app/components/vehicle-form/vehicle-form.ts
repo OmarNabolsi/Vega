dbe46e7370028e7d4d68880fec1f94e8cc4d5cc0
Omar <omar.nabolsi@gmail.com>
2018-03-04 06:54:54 -0800

    Calling the MakeService from the Component

--- ClientApp/app/components/vehicle-form/vehicle-form.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { MakeService } from './../../services/make.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'vehicle-form',
     templateUrl: 'vehicle-form.html'
 })
-export class VehicleFormComponent {
+export class VehicleFormComponent implements OnInit {
+    makes: any[];
+    models: any[];
+    vehicle: any = {};
 
+    constructor(private makeService: MakeService) {}
+
+    ngOnInit() {
+        this.makeService.getMakes()
+            .subscribe(makes => this.makes = makes);
+
+    }
+
+    onMakeChange() {
+        var selectedMake = this.makes.find(m => m.id == this.vehicle.make);
+        this.models = selectedMake? selectedMake.models : [];
+
+    }
 }
