9d6939cccbda884d49731cbe0fface571527a8a8
Omar <omar.nabolsi@gmail.com>
2018-03-05 10:37:18 -0800

    Refactoring Resources

--- Models/Resources/VehicleResource.cs
+++ Models/Resources/VehicleResource.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel.DataAnnotations;
+using vega.Models.Resources;
 
 namespace vega.Models.Resources
 {
@@ -9,18 +9,23 @@ namespace vega.Models.Resources
     {
         public int Id { get; set; }
 
-        [Required]
-        public int ModelId { get; set; }
+        public DateTime LastUpdate { get; set; }
+
+        public KeyValuePairResource Model { get; set; }
+
+        public KeyValuePairResource Make { get; set; }
 
         public bool IsRegistered { get; set; }
 
-        [Required]
-        public ContactResource Contact { get; set; }        
-        public ICollection<int> Features { get; set; }
+        public ContactResource Contact { get; set; }
+
+    
+        public ICollection<KeyValuePairResource> Features { get; set; }
 
         public VehicleResource()
         {
-            Features = new Collection<int>();
+            Features = new Collection<KeyValuePairResource>();
         }
+
     }
 }
